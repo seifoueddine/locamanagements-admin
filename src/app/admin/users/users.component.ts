7cbe344f1f49d97dbb63f201309aff74c1d13892
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2020-04-10 03:23:21 +0200

    display slug name for users list

--- src/app/admin/users/users.component.ts
+++ src/app/admin/users/users.component.ts
@@ -20,14 +20,15 @@ export class UsersComponent implements OnInit {
   active = 'id';
   direction = 'desc';
   pageIndex = 1;
-  displayedColumns = ['avatar', 'name', 'email', 'created_at','slug_id', 'role', 'action'];
+  displayedColumns = ['avatar', 'name', 'email', 'created_at', 'slug_name', 'role', 'action'];
   dataSource = new MatTableDataSource<Users>();
   elementsLength = 0;
   pageSize = 10;
   urlForAvatar = environment.URL_PATH;
+  slugs: any;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
-
+  
   openDialogForConfirmation(templateRef: TemplateRef<any>) {
     this.dialog.open(templateRef);
   }
@@ -41,9 +42,11 @@ export class UsersComponent implements OnInit {
 
   getUsers = () => {
     this.usersService.getUsers(undefined, undefined, undefined, undefined)
-      .subscribe((res: HttpResponse<Users[]>) => {
+      .subscribe((res: HttpResponse<any>) => {
         this.elementsLength = +res.headers.get('X-Total-Count');
-        this.dataSource.data = res.body;
+        const resp =  res.body;
+        this.dataSource.data = resp.data;
+        this.slugs = resp.included;
       }, error => {
         this.snackBar.open(error.error.message, 'close', { verticalPosition: 'top',  horizontalPosition: 'right' });
       });
@@ -54,9 +57,11 @@ export class UsersComponent implements OnInit {
     this.direction = event.direction;
     this.pageIndex = 1;
     this.usersService.getUsers(this.pageIndex, this.active, this.direction, undefined)
-      .subscribe((res: HttpResponse<Users[]>) => {
+      .subscribe((res: HttpResponse<any>) => {
         this.elementsLength = +res.headers.get('X-Total-Count');
-        this.dataSource.data = res.body;
+        const resp =  res.body;
+        this.dataSource.data = resp.data;
+        this.slugs = resp.included;
       }, error => {
         this.snackBar.open(error.error.message, 'close', { verticalPosition: 'top', horizontalPosition: 'right' });
       });
@@ -66,9 +71,11 @@ export class UsersComponent implements OnInit {
   getUpdate(event) {
     this.pageIndex = event.pageIndex + 1;
     this.usersService.getUsers(this.pageIndex, this.active, this.direction, undefined)
-      .subscribe((res: HttpResponse<Users[]>) => {
+      .subscribe((res: HttpResponse<any>) => {
         this.elementsLength = +res.headers.get('X-Total-Count');
-        this.dataSource.data = res.body;
+        const resp =  res.body;
+        this.dataSource.data = resp.data;
+        this.slugs = resp.included;
       }, error => {
         this.snackBar.open(error.error.message, 'close', { verticalPosition: 'top',  horizontalPosition: 'right'});
       });
@@ -105,6 +112,13 @@ export class UsersComponent implements OnInit {
         this.snackBar.open(err, 'close', { verticalPosition: 'top',  horizontalPosition: 'right'});
      
     });
+  }
+
+
+  getSlugName(slug_id){
+    let slug = this.slugs.find(x => +x.id === slug_id);
+    return slug.attributes.Name
+    
   }
 
 
